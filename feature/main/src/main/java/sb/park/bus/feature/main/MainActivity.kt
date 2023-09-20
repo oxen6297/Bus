414fa1dd57f0a57b20cc26b9071ee8564cf196b2
oxen6297 <112065694+oxen6297@users.noreply.github.com>
2023-09-20 16:47:47 +0900

    feat: 검색창 구현

--- feature/main/src/main/java/sb/park/bus/feature/main/MainActivity.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/MainActivity.kt
@@ -3,13 +3,29 @@ package sb.park.bus.feature.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import sb.park.bus.feature.main.theme.BusTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,25 +37,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SearchTextField()
                 }
             }
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+    private fun SearchTextField() {
+        var text by remember { mutableStateOf(TextFieldValue("")) }
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            TextField(
+                value = text,
+                onValueChange = { newText -> text = newText },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                singleLine = true,
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.text_hint),
+                        style = TextStyle(
+                            color = Color.LightGray,
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.Light
+                        )
+                    )
+                }
+            )
+        }
     }
 
     @Preview(showBackground = true)
     @Composable
     fun GreetingPreview() {
         BusTheme {
-            Greeting("Android")
+            SearchTextField()
         }
     }
 }
