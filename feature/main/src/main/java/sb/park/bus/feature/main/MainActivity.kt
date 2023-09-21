21c50dbd36fc9cea64440c8d658a4e125bd657e2
oxen6297 <oxen6297@naver.com>
2023-09-21 18:51:28 +0900

    feat: 검색창 구현 및 검색 버튼 구현

--- feature/main/src/main/java/sb/park/bus/feature/main/MainActivity.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/MainActivity.kt
@@ -3,22 +3,36 @@ package sb.park.bus.feature.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -43,32 +57,71 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
     @Composable
     private fun SearchTextField() {
         var text by remember { mutableStateOf(TextFieldValue("")) }
+        val keyboardController = LocalSoftwareKeyboardController.current
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 20.dp, end = 20.dp, top = 20.dp)
         ) {
-            TextField(
-                value = text,
-                onValueChange = { newText -> text = newText },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-                singleLine = true,
-                placeholder = {
-                    Text(
-                        text = stringResource(id = R.string.text_hint),
-                        style = TextStyle(
+            Box(modifier = Modifier.fillMaxWidth()) {
+                TextField(
+                    value = text,
+                    onValueChange = { newText -> text = newText },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .border(
+                            width = 1.dp,
                             color = Color.LightGray,
-                            fontSize = 13.sp,
-                            fontWeight = FontWeight.Light
+                            shape = RoundedCornerShape(15.dp)
+                        ),
+                    singleLine = true,
+                    placeholder = {
+                        Text(
+                            text = stringResource(id = R.string.text_hint),
+                            style = TextStyle(
+                                color = Color.LightGray,
+                                fontSize = 13.sp,
+                                fontWeight = FontWeight.Light
+                            )
                         )
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.White,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        cursorColor = Color.Gray
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            clickSearchBtn()
+                            keyboardController?.hide()
+                        }
+                    )
+                )
+                IconButton(
+                    onClick = { clickSearchBtn() },
+                    modifier = Modifier
+                        .size(48.dp)
+                        .align(Alignment.CenterEnd)
+                        .padding(end = 15.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = null,
+                        tint = Color.Gray
                     )
                 }
-            )
+            }
         }
+    }
+
+    private fun clickSearchBtn() {
+        //TODO 검색 버튼 클릭 로직 작성
     }
 
     @Preview(showBackground = true)
