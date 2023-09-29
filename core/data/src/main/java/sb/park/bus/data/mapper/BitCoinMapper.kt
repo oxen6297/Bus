ceb2dafad66c00cfba504fa36d86bb3bd67c8896
oxen6297 <oxen6297@naver.com>
2023-09-29 18:09:01 +0900

    feat: 비트코인 mapper 수정 및 구현

--- core/data/src/main/java/sb/park/bus/data/mapper/BitCoinMapper.kt
+++ core/data/src/main/java/sb/park/bus/data/mapper/BitCoinMapper.kt
@@ -2,11 +2,23 @@ package sb.park.bus.data.mapper
 
 import android.annotation.SuppressLint
 import sb.park.bus.data.model.BitCoinModel
+import sb.park.bus.data.model.CoinBaseModel
 import java.text.SimpleDateFormat
+import java.util.Date
+
+internal fun CoinBaseModel.toData(): CoinBaseModel = CoinBaseModel(
+    status = this.status,
+    data = this.data.toData()
+)
 
 @SuppressLint("SimpleDateFormat")
-internal fun BitCoinModel.toData(): BitCoinModel = BitCoinModel(
-    status = this.status,
-    data = this.data,
-    date = SimpleDateFormat("yyyy-MM-dd").format(this.date)
-)
+internal fun BitCoinModel.toData(): BitCoinModel {
+    val regex = "(\\d)(?=(\\d{3})+\$)".toRegex()
+
+    return BitCoinModel(
+        changeRatio = "${this.changeRatio}%",
+        maxPrice = "${this.maxPrice.replace(regex,"\$1,")} 원",
+        minPrice = "${this.minPrice.replace(regex,"\$1,")} 원",
+        date = SimpleDateFormat("yyyy-MM-dd").format(Date(this.date.toLong()))
+    )
+}
