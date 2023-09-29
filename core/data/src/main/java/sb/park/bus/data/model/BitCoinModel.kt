664d7801204aa899d1af751539444eea33be55ae
oxen6297 <oxen6297@naver.com>
2023-09-29 18:08:17 +0900

    refactor: response model 수정

--- core/data/src/main/java/sb/park/bus/data/model/BitCoinModel.kt
+++ core/data/src/main/java/sb/park/bus/data/model/BitCoinModel.kt
@@ -3,14 +3,8 @@ package sb.park.bus.data.model
 import com.google.gson.annotations.SerializedName
 
 data class BitCoinModel(
-    val status: String,
-    val data: List<DataModel>,
+    @SerializedName("fluctate_rate_24H") val changeRatio: String,
+    @SerializedName("max_price") val maxPrice: String,
+    @SerializedName("min_price") val minPrice: String,
     val date: String
-    ) {
-    data class DataModel(
-        @SerializedName("units_traded_24H") val unitsTraded: String,
-        @SerializedName("acc_trade_value_24H") val accTradeValue: String,
-        @SerializedName("fluctate_24H") val changeRate: String,
-        @SerializedName("fluctate_rate_24H") val changeRatio: String,
-    )
-}
+)
---
+++ core/data/src/main/java/sb/park/bus/data/model/CoinBaseModel.kt
@@ -0,0 +1,6 @@
+package sb.park.bus.data.model
+
+data class CoinBaseModel(
+    val status: String,
+    val data: BitCoinModel
+)
