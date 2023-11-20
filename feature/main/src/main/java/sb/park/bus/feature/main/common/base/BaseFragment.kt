e1a901b445b6f028e03bdc1bff463c2ee28544e7
oxen6297 <oxen6297@naver.com>
2023-11-20 20:39:46 +0900

    feat: baseActivity, baseFragment 구현

---
+++ feature/main/src/main/java/sb/park/bus/feature/main/common/base/BaseFragment.kt
@@ -0,0 +1,37 @@
+package sb.park.bus.feature.main.common.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding>(private val layoutId: Int) : Fragment() {
+
+    private var _binding: B? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        init(view)
+    }
+
+    protected abstract fun init(view: View)
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
