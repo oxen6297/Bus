1b41b15f9c6f7369967dde8ab7331ec37ebb1506
oxen6297 <oxen6297@naver.com>
2023-11-20 20:40:00 +0900

    feat: timber Logger 구현

---
+++ feature/main/src/main/java/sb/park/bus/feature/main/common/Logger.kt
@@ -0,0 +1,9 @@
+package sb.park.bus.feature.main.common
+
+import timber.log.Timber
+
+inline fun <reified T> T.debug(message: String) = Timber.d(message)
+inline fun <reified T> T.error(message: String) = Timber.e(message)
+inline fun <reified T> T.warn(message: String) = Timber.w(message)
+inline fun <reified T> T.info(message: String) = Timber.i(message)
+inline fun <reified T> T.error(exception: Exception) = Timber.e(exception)
