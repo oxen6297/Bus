c208ddec7b7c5717f434a38ea07da3d757525b55
oxen6297 <oxen6297@naver.com>
2023-12-07 19:46:34 +0900

    refactor: timber 초기화

--- app/src/main/java/sb/park/bus/ui/HiltApplication.kt
+++ app/src/main/java/sb/park/bus/ui/HiltApplication.kt
@@ -3,6 +3,13 @@ package sb.park.bus.ui
 import android.app.Application
 import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class HiltApplication : Application()
+class HiltApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
