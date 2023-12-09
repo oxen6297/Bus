0ba35ad2762fa8926159fb04515c9cf15daf1bc4
oxen6297 <oxen6297@naver.com>
2023-12-09 21:45:00 +0900

    refactor: @LayoutRes annotation 추가 및 init abstract fun 삭제

--- feature/main/src/main/java/sb/park/bus/feature/main/common/base/BaseFragment.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/common/base/BaseFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment<B : ViewDataBinding>(private val layoutId: Int) : Fragment() {
+abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
+    Fragment() {
 
     private var _binding: B? = null
     protected val binding get() = _binding!!
@@ -25,10 +27,7 @@ abstract class BaseFragment<B : ViewDataBinding>(private val layoutId: Int) : Fr
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
-        init(view)
     }
-
-    protected abstract fun init(view: View)
 
     override fun onDestroy() {
         super.onDestroy()
