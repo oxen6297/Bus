07604b326cdae0593109f92d5221b14b0c5e4f57
oxen6297 <oxen6297@naver.com>
2023-12-09 21:43:05 +0900

    feat: HomeFragment 추가

---
+++ feature/main/src/main/java/sb/park/bus/feature/main/views/fragments/HomeFragment.kt
@@ -0,0 +1,14 @@
+package sb.park.bus.feature.main.views.fragments
+
+import android.os.Bundle
+import android.view.View
+import sb.park.bus.feature.main.R
+import sb.park.bus.feature.main.common.base.BaseFragment
+import sb.park.bus.feature.main.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
