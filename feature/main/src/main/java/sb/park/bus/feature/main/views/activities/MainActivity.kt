6a58db636c8a70fde4c085043acaa4843e630bcc
oxen6297 <oxen6297@naver.com>
2023-12-12 19:23:30 +0900

    refactor: 미사용 import 제거

--- feature/main/src/main/java/sb/park/bus/feature/main/views/activities/MainActivity.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/views/activities/MainActivity.kt
@@ -1,7 +1,6 @@
 package sb.park.bus.feature.main.views.activities
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import dagger.hilt.android.AndroidEntryPoint
 import sb.park.bus.feature.main.R
 import sb.park.bus.feature.main.common.base.BaseActivity
