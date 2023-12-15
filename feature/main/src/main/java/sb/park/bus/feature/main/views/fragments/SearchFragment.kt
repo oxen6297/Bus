8d0b4600bba98430311524c9d53dcc389a8561a5
oxen6297 <oxen6297@naver.com>
2023-12-15 16:33:04 +0900

    feat: SearchFragment 생성

---
+++ feature/main/src/main/java/sb/park/bus/feature/main/views/fragments/SearchFragment.kt
@@ -0,0 +1,16 @@
+package sb.park.bus.feature.main.views.fragments
+
+import android.os.Bundle
+import android.view.View
+import sb.park.bus.feature.main.R
+import sb.park.bus.feature.main.common.base.BaseFragment
+import sb.park.bus.feature.main.databinding.FragmentSearchBinding
+
+class SearchFragment:BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
