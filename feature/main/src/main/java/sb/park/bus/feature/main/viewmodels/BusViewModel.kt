eb2d7e04c19655bff1be9ca78b8636540ff22567
oxen6297 <oxen6297@naver.com>
2023-12-18 18:52:16 +0900

    refactor: busList, stationList 메소드 구현

--- feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/BusViewModel.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/BusViewModel.kt
@@ -1,11 +1,47 @@
 package sb.park.bus.feature.main.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import sb.park.bus.domain.usecase.BusIdUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import sb.park.bus.data.repository.BusIdRepository
+import sb.park.bus.data.repository.BusStationRepository
+import sb.park.bus.data.response.BusIdResponse
+import sb.park.bus.data.response.BusStationResponse
+import sb.park.bus.feature.main.ApiResult
 import javax.inject.Inject
 
 @HiltViewModel
-class BusViewModel @Inject constructor(private val busIdUseCase: BusIdUseCase) : ViewModel() {
-    
+class BusViewModel @Inject constructor(
+    private val busIdRepository: BusIdRepository,
+    private val busStationRepository: BusStationRepository
+) : ViewModel() {
+
+    private val _bus = MutableStateFlow<ApiResult<List<BusIdResponse>>>(ApiResult.Loading)
+    val bus = _bus.asStateFlow()
+
+    private val _station = MutableStateFlow<ApiResult<List<BusStationResponse>>>(ApiResult.Loading)
+    val station = _station.asStateFlow()
+
+    fun getBusList(busNumber: String) {
+        viewModelScope.launch {
+            try {
+                _bus.emit(ApiResult.Success(busIdRepository.getData(busNumber)))
+            } catch (e: Exception) {
+                _bus.emit(ApiResult.Error(e))
+            }
+        }
+    }
+
+    fun getStationList(busId: String) {
+        viewModelScope.launch {
+            try {
+                _station.emit(ApiResult.Success(busStationRepository.getData(busId)))
+            } catch (e: Exception) {
+                _station.emit(ApiResult.Error(e))
+            }
+        }
+    }
 }
