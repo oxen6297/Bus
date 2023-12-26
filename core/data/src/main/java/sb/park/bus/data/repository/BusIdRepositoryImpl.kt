7bcc24d5469fd01b0ded72ce99cc855f66ed04cc
oxen6297 <oxen6297@naver.com>
2023-12-26 19:19:44 +0900

    refactor: repository 수정

--- core/data/src/main/java/sb/park/bus/data/repository/BusIdRepositoryImpl.kt
+++ core/data/src/main/java/sb/park/bus/data/repository/BusIdRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package sb.park.bus.data.repository
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import sb.park.bus.data.response.BusIdResponse
 import sb.park.bus.data.service.BusIdService
 import javax.inject.Inject
@@ -7,7 +9,10 @@ import javax.inject.Inject
 internal class BusIdRepositoryImpl @Inject constructor(private val busIdService: BusIdService) :
     BusIdRepository {
     override suspend fun getData(busNumber: String): List<BusIdResponse> {
-        return busIdService.getBusId().filter {
+        return Gson().fromJson<List<BusIdResponse>>(
+            busIdService.getBusId(),
+            object : TypeToken<List<BusIdResponse>>() {}.type
+        ).filter {
             it.routeName == busNumber
         }.distinctBy {
             it.routeId
--- core/data/src/main/java/sb/park/bus/data/repository/BusStationRepositoryImpl.kt
+++ core/data/src/main/java/sb/park/bus/data/repository/BusStationRepositoryImpl.kt
@@ -1,11 +1,19 @@
 package sb.park.bus.data.repository
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import sb.park.bus.data.response.BusStationResponse
 import sb.park.bus.data.service.BusStationService
 import javax.inject.Inject
 
 class BusStationRepositoryImpl @Inject constructor(private val busStationService: BusStationService) :
     BusStationRepository {
-    override suspend fun getData(busId: String): List<BusStationResponse> =
-        busStationService.getData(busRouteId = busId)
+    override suspend fun getData(busId: String): List<BusStationResponse> {
+        return Gson().fromJson<List<BusStationResponse>?>(
+            busStationService.getData(busRouteId = busId).msgBody.itemList,
+            object : TypeToken<List<BusStationResponse>>() {}.type
+        ).distinctBy {
+            it.direction
+        }
+    }
 }
