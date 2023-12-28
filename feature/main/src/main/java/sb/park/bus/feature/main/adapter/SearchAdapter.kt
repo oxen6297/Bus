642076598b1fa35719a70310cccf49a2287bab45
sbpark <112065694+oxen6297@users.noreply.github.com>
2023-12-28 12:18:30 +0900

    refactor: response 수정

--- feature/main/src/main/java/sb/park/bus/feature/main/adapter/SearchAdapter.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/adapter/SearchAdapter.kt
@@ -5,10 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import sb.park.bus.data.response.BusStationResponse
+import sb.park.bus.data.response.BusSearchResponse
 import sb.park.bus.feature.main.databinding.ItemBusSearchBinding
 
-class SearchAdapter : ListAdapter<BusStationResponse, SearchAdapter.ViewHolder>(DiffCallback()) {
+class SearchAdapter : ListAdapter<BusSearchResponse, SearchAdapter.ViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -23,24 +23,22 @@ class SearchAdapter : ListAdapter<BusStationResponse, SearchAdapter.ViewHolder>(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.apply {
             bus = getItem(position)
-            textStartStation.text = getItem(0).direction
-            textEndStation.text = getItem(itemCount - 1).direction
         }
     }
 
     class ViewHolder(val binding: ItemBusSearchBinding) : RecyclerView.ViewHolder(binding.root)
 
-    class DiffCallback : DiffUtil.ItemCallback<BusStationResponse>() {
+    class DiffCallback : DiffUtil.ItemCallback<BusSearchResponse>() {
         override fun areItemsTheSame(
-            oldItem: BusStationResponse,
-            newItem: BusStationResponse
+            oldItem: BusSearchResponse,
+            newItem: BusSearchResponse
         ): Boolean {
             return oldItem.busRouteNm == newItem.busRouteNm
         }
 
         override fun areContentsTheSame(
-            oldItem: BusStationResponse,
-            newItem: BusStationResponse
+            oldItem: BusSearchResponse,
+            newItem: BusSearchResponse
         ): Boolean {
             return oldItem == newItem
         }
