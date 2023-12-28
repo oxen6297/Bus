ad886b5afd90a74a1c2e1ffdb8a4d2065ab2a320
sbpark <112065694+oxen6297@users.noreply.github.com>
2023-12-28 12:17:33 +0900

    feat: search mapper 구현

---
+++ core/data/src/main/java/sb/park/bus/data/mapper/BusSearchMapper.kt
@@ -0,0 +1,14 @@
+package sb.park.bus.data.mapper
+
+import sb.park.bus.data.response.BusSearchResponse
+import sb.park.bus.data.response.BusStationResponse
+
+internal fun List<BusStationResponse>.toSearch(): List<BusSearchResponse> {
+    return listOf(
+        BusSearchResponse(
+            busRouteNm = get(0).busRouteNm,
+            startDirection = get(0).direction,
+            endDirection = get(lastIndex).direction
+        )
+    )
+}
