6adbf3ba6a507d4c3439288dce268745a225d280
sbpark <112065694+oxen6297@users.noreply.github.com>
2023-12-28 12:17:56 +0900

    refactor: getSearch 메소드 구현

--- core/data/src/main/java/sb/park/bus/data/repository/BusStationRepository.kt
+++ core/data/src/main/java/sb/park/bus/data/repository/BusStationRepository.kt
@@ -1,7 +1,10 @@
 package sb.park.bus.data.repository
 
+import sb.park.bus.data.response.BusSearchResponse
 import sb.park.bus.data.response.BusStationResponse
 
 interface BusStationRepository {
     suspend fun getData(busId: String): List<BusStationResponse>
+
+    suspend fun getSearch(busId: String): List<BusSearchResponse>
 }
