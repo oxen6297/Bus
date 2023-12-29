9ef18b27f238217d416ed91118f9ae47371408bd
sbpark <112065694+oxen6297@users.noreply.github.com>
2023-12-29 14:56:43 +0900

    refactor: apiResult 확장함수 구현

--- core/data/src/main/java/sb/park/bus/data/ApiResult.kt
+++ core/data/src/main/java/sb/park/bus/data/ApiResult.kt
@@ -1,5 +1,9 @@
 package sb.park.bus.data
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onStart
+
 sealed interface ApiResult<out T> {
     object Loading : ApiResult<Nothing>
     data class Success<out T>(val data: T) : ApiResult<T>
@@ -22,4 +26,14 @@ inline fun <reified T> ApiResult<T>.onError(failed: (Throwable) -> Unit) {
     if (this is ApiResult.Error) {
         failed(e)
     }
+}
+
+fun <T> safeFlow(service: suspend () -> T): Flow<ApiResult<T>> = flow {
+    try {
+        emit(ApiResult.Success(service()))
+    } catch (e: Exception) {
+        emit(ApiResult.Error(e))
+    }
+}.onStart {
+    emit(ApiResult.Loading)
 }
