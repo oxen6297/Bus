e76a0c2f1035bd9f6cf0a5a2414f43155e7a3c8a
sbpark <112065694+oxen6297@users.noreply.github.com>
2023-12-29 14:56:54 +0900

    refactor: repo 수정

--- core/data/src/main/java/sb/park/bus/data/repository/BitCoinRepository.kt
+++ core/data/src/main/java/sb/park/bus/data/repository/BitCoinRepository.kt
@@ -1,7 +1,9 @@
 package sb.park.bus.data.repository
 
+import kotlinx.coroutines.flow.Flow
+import sb.park.bus.data.ApiResult
 import sb.park.bus.data.response.BaseResponse
 
 interface BitCoinRepository {
-    suspend fun getData(): BaseResponse
+    suspend fun getData(): Flow<ApiResult<BaseResponse>>
 }
--- core/data/src/main/java/sb/park/bus/data/repository/BitCoinRepositoryImpl.kt
+++ core/data/src/main/java/sb/park/bus/data/repository/BitCoinRepositoryImpl.kt
@@ -1,11 +1,22 @@
 package sb.park.bus.data.repository
 
-import sb.park.bus.data.service.BitCoinService
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import sb.park.bus.data.ApiResult
+import sb.park.bus.data.AppDispatchers
+import sb.park.bus.data.Dispatcher
 import sb.park.bus.data.mapper.toData
 import sb.park.bus.data.response.BaseResponse
+import sb.park.bus.data.safeFlow
+import sb.park.bus.data.service.BitCoinService
 import javax.inject.Inject
 
-internal class BitCoinRepositoryImpl @Inject constructor(private val bitCoinService: BitCoinService) :
-    BitCoinRepository {
-    override suspend fun getData(): BaseResponse = bitCoinService.getData().toData()
+internal class BitCoinRepositoryImpl @Inject constructor(
+    private val bitCoinService: BitCoinService,
+    @Dispatcher(AppDispatchers.IO) private val coroutineDispatcher: CoroutineDispatcher
+) : BitCoinRepository {
+    override suspend fun getData(): Flow<ApiResult<BaseResponse>> = safeFlow {
+        bitCoinService.getData().toData()
+    }.flowOn(coroutineDispatcher)
 }
