82a62f0e23c9f5f5cda99089b0f06fdfcea0ff19
oxen6297 <oxen6297@naver.com>
2024-01-01 14:49:05 +0900

    refactor: return type Flow

--- core/data/src/main/java/sb/park/bus/data/repository/BusIdRepository.kt
+++ core/data/src/main/java/sb/park/bus/data/repository/BusIdRepository.kt
@@ -1,7 +1,9 @@
 package sb.park.bus.data.repository
 
+import kotlinx.coroutines.flow.Flow
+import sb.park.bus.data.ApiResult
 import sb.park.bus.data.response.BusIdResponse
 
 interface BusIdRepository {
-    suspend fun getData(busNumber: String): List<BusIdResponse>
+    suspend fun getData(busNumber: String): Flow<ApiResult<List<BusIdResponse>>>
 }
--- core/data/src/main/java/sb/park/bus/data/repository/BusStationRepository.kt
+++ core/data/src/main/java/sb/park/bus/data/repository/BusStationRepository.kt
@@ -1,10 +1,12 @@
 package sb.park.bus.data.repository
 
+import kotlinx.coroutines.flow.Flow
+import sb.park.bus.data.ApiResult
 import sb.park.bus.data.response.BusSearchResponse
 import sb.park.bus.data.response.BusStationResponse
 
 interface BusStationRepository {
-    suspend fun getData(busId: String): List<BusStationResponse>
+    suspend fun getData(busId: String): Flow<ApiResult<List<BusStationResponse>>>
 
-    suspend fun getSearch(busId: String): List<BusSearchResponse>
+    suspend fun getSearch(busId: String): Flow<ApiResult<List<BusSearchResponse>>>
 }
