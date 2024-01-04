82b1c2637473308ca0e3e84d55edb0821f680b6a
oxen6297 <oxen6297@naver.com>
2024-01-04 19:06:07 +0900

    feat: recyclerView adapter, submitList 바인딩 어댑터 구현

--- feature/main/src/main/java/sb/park/bus/feature/main/utils/BindingAdapters.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/utils/BindingAdapters.kt
@@ -3,6 +3,8 @@ package sb.park.bus.feature.main.utils
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import sb.park.bus.data.ApiResult
 
 object BindingAdapters {
@@ -19,5 +21,18 @@ object BindingAdapters {
         throwable?.message?.let { errorMessage ->
             Toast.makeText(view.context, errorMessage, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    @JvmStatic
+    @BindingAdapter("adapter")
+    fun bindAdapter(recyclerView: RecyclerView, adapter: RecyclerView.Adapter<*>) {
+        recyclerView.adapter = adapter
+    }
+
+    @JvmStatic
+    @BindingAdapter("submitList")
+    @Suppress("UNCHECKED_CAST")
+    fun bindSubmitList(view: RecyclerView, itemList: List<Any>?) {
+        (view.adapter as ListAdapter<Any, *>).submitList(itemList)
     }
 }
