c41fe1c7270bb39bfb6c89af9eedd0c233a3725e
oxen6297 <oxen6297@naver.com>
2024-01-09 20:47:59 +0900

    refactor: ApiResult Success T nullable

--- core/data/src/main/java/sb/park/bus/data/ApiResult.kt
+++ core/data/src/main/java/sb/park/bus/data/ApiResult.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.onStart
 
 sealed class ApiResult<out T> {
     object Loading : ApiResult<Nothing>()
-    data class Success<out T>(val data: T) : ApiResult<T>()
+    data class Success<out T>(val data: T?) : ApiResult<T>()
     data class Error(val e: Throwable) : ApiResult<Nothing>()
 }
 
