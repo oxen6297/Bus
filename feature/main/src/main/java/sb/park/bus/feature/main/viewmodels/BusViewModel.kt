aeba4f1b8088d626c3500a38a47c8faea58d16f9
oxen6297 <oxen6297@naver.com>
2024-01-11 20:20:39 +0900

    refactor: viewModel 수정

--- feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/BusViewModel.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/BusViewModel.kt
@@ -4,52 +4,37 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import sb.park.bus.data.ApiResult
 import sb.park.bus.data.repository.BusIdRepository
-import sb.park.bus.data.repository.BusStationRepository
-import sb.park.bus.data.response.BusSearchResponse
+import sb.park.bus.data.response.BusIdResponse
 import sb.park.bus.data.successOrNull
 import javax.inject.Inject
 
 @HiltViewModel
-class BusViewModel @Inject constructor(
-    private val busIdRepository: BusIdRepository,
-    private val busStationRepository: BusStationRepository
-) : ViewModel() {
+class BusViewModel @Inject constructor(private val busIdRepository: BusIdRepository) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<ApiResult<Any>>(ApiResult.Loading)
-    val uiState = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow<ApiResult<List<BusIdResponse>>>(ApiResult.Loading)
+    val uiState: StateFlow<ApiResult<List<BusIdResponse>>>
+        get() = _uiState
 
-    private val _busStationFlow = MutableStateFlow<List<BusSearchResponse>?>(emptyList())
-    val busStationFlow = _busStationFlow.asStateFlow()
+    private val _busIdFlow = MutableStateFlow<List<BusIdResponse>?>(null)
+    val busIdFlow: StateFlow<List<BusIdResponse>?>
+        get() = _busIdFlow
 
-    fun getData(text: String) {
+    fun getSearch(busNumber: String) {
         viewModelScope.launch {
-            busIdRepository.getData(text).collectLatest { idState ->
-                when (idState) {
+            busIdRepository.getData(busNumber).collectLatest { result ->
+                when (result) {
                     is ApiResult.Loading -> _uiState.emit(ApiResult.Loading)
-                    is ApiResult.Error -> _uiState.emit(ApiResult.Error(idState.e))
                     is ApiResult.Success -> {
-                        if (idState.successOrNull().isNullOrEmpty()) {
-                            _busStationFlow.emit(emptyList())
-                            _uiState.emit(ApiResult.Success(null))
-                        }
-                        idState.successOrNull()?.forEach { response ->
-                            busStationRepository.getSearch(response.routeId.toString())
-                                .collectLatest { searchState ->
-                                    if (searchState is ApiResult.Error) {
-                                        _uiState.emit(ApiResult.Error(searchState.e))
-                                    }
-                                    if (searchState is ApiResult.Success) {
-                                        _busStationFlow.emit(searchState.successOrNull())
-                                        _uiState.emit(ApiResult.Success(searchState.data))
-                                    }
-                                }
-                        }
+                        _uiState.emit(ApiResult.Success(result.successOrNull()))
+                        _busIdFlow.emit(result.successOrNull())
                     }
+
+                    is ApiResult.Error -> _uiState.emit(ApiResult.Error(result.e))
                 }
             }
         }
