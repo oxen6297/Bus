254a00babf024cd4ee8f06ae001c627df784c67c
oxen6297 <oxen6297@naver.com>
2024-01-16 20:10:19 +0900

    refactor: 버스 타입에 따른 background 설정 및 corner radius 추가

--- feature/main/src/main/java/sb/park/bus/feature/main/adapter/SearchAdapter.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/adapter/SearchAdapter.kt
@@ -1,11 +1,16 @@
 package sb.park.bus.feature.main.adapter
 
+import android.graphics.Outline
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.ViewOutlineProvider
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import sb.park.bus.data.BusType
 import sb.park.bus.data.response.BusSearchResponse
+import sb.park.bus.feature.main.R
 import sb.park.bus.feature.main.databinding.ItemBusSearchBinding
 
 class SearchAdapter : ListAdapter<BusSearchResponse, SearchAdapter.ViewHolder>(diffCallback) {
@@ -23,6 +28,28 @@ class SearchAdapter : ListAdapter<BusSearchResponse, SearchAdapter.ViewHolder>(d
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.apply {
             bus = getItem(position)
+            val busTypeColor = when (bus?.routeType) {
+                BusType.PORT.typeName -> R.color.sky_blue
+                BusType.COUNTRY.typeName -> R.color.yellow
+                BusType.GANSEON.typeName -> R.color.blue
+                BusType.JISEON.typeName -> R.color.green
+                BusType.CYCLE.typeName -> R.color.red
+                BusType.WIDE.typeName -> R.color.purple
+                BusType.INCHEON.typeName -> R.color.mint
+                BusType.GYUNGGI.typeName -> R.color.black
+                BusType.COMMON.typeName -> R.color.dark_gray
+                else -> R.color.black
+            }
+
+            textBusType.apply {
+                setBackgroundColor(root.context.getColor(busTypeColor))
+                outlineProvider = object : ViewOutlineProvider() {
+                    override fun getOutline(view: View?, outline: Outline?) {
+                        view?.clipToOutline = true
+                        outline?.setRoundRect(0, 0, view!!.width, view.height, 10f)
+                    }
+                }
+            }
         }
     }
 
