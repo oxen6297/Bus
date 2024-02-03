f93cb0ce521435d0ea3475b3e74b4fa9c5b7801b
oxen6297 <oxen6297@naver.com>
2024-02-03 17:07:05 +0900

    refactor: SavedStateHandle 수정 및 HiltViewModel 어노테이션 추가

--- feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/DetailViewModel.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/DetailViewModel.kt
@@ -3,24 +3,27 @@ package sb.park.bus.feature.main.viewmodels
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import sb.park.domain.usecases.BusStationUseCase
 import sb.park.model.ApiResult
+import sb.park.model.response.BusSearchResponse
 import sb.park.model.successOrNull
 import javax.inject.Inject
 
+@HiltViewModel
 class DetailViewModel @Inject constructor(
     busStationUseCase: BusStationUseCase,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    private val busId by lazy {
-        savedStateHandle.get<String>("busId") ?: throw NullPointerException()
+    private val bus by lazy {
+        savedStateHandle.get<BusSearchResponse>("bus") ?: throw NullPointerException()
     }
 
-    val uiState = busStationUseCase(busId).stateIn(
+    val uiState = busStationUseCase(bus.busId).stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5000L),
         initialValue = ApiResult.Loading
