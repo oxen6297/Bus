03f4a16b38d11374b758de6e9bde1b00e65c894b
oxen6297 <oxen6297@naver.com>
2024-02-03 17:07:17 +0900

    refactor: viewModel binding

--- feature/main/src/main/java/sb/park/bus/feature/main/views/fragments/DetailFragment.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/views/fragments/DetailFragment.kt
@@ -1,19 +1,22 @@
 package sb.park.bus.feature.main.views.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import sb.park.bus.feature.main.R
 import sb.park.bus.feature.main.common.base.BaseFragment
 import sb.park.bus.feature.main.databinding.FragmentDetailBinding
+import sb.park.bus.feature.main.viewmodels.DetailViewModel
 
 @AndroidEntryPoint
 class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
 
+    private val viewModel: DetailViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.vm = viewModel
     }
 }
