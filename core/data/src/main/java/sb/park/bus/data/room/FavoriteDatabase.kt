88b7f77d8b9435e2ea064619a028ac817afb60f2
oxen6297 <oxen6297@naver.com>
2024-02-08 16:23:42 +0900

    refactor: move FavoriteEntity module

--- core/data/src/main/java/sb/park/bus/data/room/FavoriteDatabase.kt
+++ core/data/src/main/java/sb/park/bus/data/room/FavoriteDatabase.kt
@@ -2,6 +2,7 @@ package sb.park.bus.data.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import sb.park.model.response.FavoriteEntity
 
 @Database(entities = [FavoriteEntity::class], version = 1)
 abstract class FavoriteDatabase : RoomDatabase() {
