f80a7390f767c17b39442a0bdd711c492bb2b5ac
oxen6297 <oxen6297@naver.com>
2024-02-08 17:26:44 +0900

    feat: customDialog extension 구현

--- feature/main/src/main/java/sb/park/bus/feature/main/extensions/Extensions.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/extensions/Extensions.kt
@@ -3,12 +3,8 @@ package sb.park.bus.feature.main.extensions
 import android.content.Context
 import android.view.View
 import android.widget.Toast
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import sb.park.bus.feature.main.R
+import sb.park.bus.feature.main.utils.CustomDialogBuilder
 import sb.park.bus.feature.main.utils.SingleClick
 
 fun View.hide() {
@@ -19,10 +15,6 @@ fun View.show() {
     visibility = View.VISIBLE
 }
 
-fun View.isShow(): Boolean {
-    return visibility == View.VISIBLE
-}
-
 fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
 }
@@ -31,8 +23,16 @@ fun View.singleClickListener(click: (View) -> Unit) {
     setOnClickListener(SingleClick(click))
 }
 
-fun LifecycleOwner.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
-    lifecycleScope.launch {
-        repeatOnLifecycle(Lifecycle.State.STARTED, block)
-    }
+fun Context.customDialog(
+    titleText: String,
+    confirmBtnText: String = "확인",
+    cancelBtnText: String = "취소",
+    clickCancel: (View) -> Unit = {},
+    clickConfirm: (View) -> Unit
+): CustomDialogBuilder = CustomDialogBuilder(this).apply {
+    initView(R.id.btn_confirm, R.id.btn_cancel, R.id.text_title)
+    setViewText(titleText, confirmBtnText, cancelBtnText)
+    clickCancel(clickCancel)
+    clickConfirm(clickConfirm)
+    showDialog()
 }
