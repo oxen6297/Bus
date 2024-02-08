ffa21522ad0bf4079f5e537c841308d7c6682f09
oxen6297 <oxen6297@naver.com>
2024-02-08 17:26:33 +0900

    feat: 즐겨찾기 삭제 및 추가 로직 구현

--- feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/DetailViewModel.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/DetailViewModel.kt
@@ -5,24 +5,45 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import sb.park.domain.usecases.BusStationUseCase
+import sb.park.domain.usecases.FavoriteUseCase
 import sb.park.model.ApiResult
 import sb.park.model.response.BusSearchResponse
+import sb.park.model.response.FavoriteEntity
 import sb.park.model.successOrNull
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
     busStationUseCase: BusStationUseCase,
+    private val favoriteUseCase: FavoriteUseCase,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
     private val _bus = savedStateHandle.getLiveData<BusSearchResponse>("bus")
     val bus: LiveData<BusSearchResponse>
         get() = _bus
+
+    private val _isFavorite = MutableStateFlow(false)
+    val isFavorite = _isFavorite.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            favoriteUseCase.getFavorite().forEach {
+                if (it.busId == bus.value?.busId) {
+                    _isFavorite.emit(true)
+                } else {
+                    _isFavorite.emit(false)
+                }
+            }
+        }
+    }
 
     val uiState = busStationUseCase(bus.value?.busId!!).stateIn(
         scope = viewModelScope,
@@ -35,4 +56,23 @@ class DetailViewModel @Inject constructor(
         started = SharingStarted.WhileSubscribed(5000L),
         initialValue = null
     )
+
+    fun deleteFavorite() {
+        viewModelScope.launch {
+            _isFavorite.emit(false)
+            favoriteUseCase.deleteFavorite(bus.value?.busId!!)
+        }
+    }
+
+    fun addFavorite() {
+        viewModelScope.launch {
+            _isFavorite.emit(true)
+            favoriteUseCase.insertFavorite(
+                FavoriteEntity(
+                    busNumber = bus.value?.busRouteNm!!,
+                    busId = bus.value?.busId!!
+                )
+            )
+        }
+    }
 }
--- feature/main/src/main/java/sb/park/bus/feature/main/views/fragments/DetailFragment.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/views/fragments/DetailFragment.kt
@@ -3,11 +3,15 @@ package sb.park.bus.feature.main.views.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import sb.park.bus.feature.main.R
 import sb.park.bus.feature.main.adapter.StationAdapter
 import sb.park.bus.feature.main.common.base.BaseFragment
 import sb.park.bus.feature.main.databinding.FragmentDetailBinding
+import sb.park.bus.feature.main.extensions.customDialog
+import sb.park.bus.feature.main.extensions.showToast
+import sb.park.bus.feature.main.utils.StationItemDecoration
 import sb.park.bus.feature.main.viewmodels.DetailViewModel
 
 @AndroidEntryPoint
@@ -21,6 +25,22 @@ class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_det
         binding.apply {
             vm = viewModel
             adapter = StationAdapter()
+            recyclerviewStation.addItemDecoration(StationItemDecoration())
+
+            btnBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+
+            btnFavorite.setOnClickListener {
+                if (viewModel.isFavorite.value) {
+                    view.context.customDialog("즐겨찾기에서 삭제하시겠습니까?") {
+                        viewModel.deleteFavorite()
+                    }
+                } else {
+                    view.context.showToast("즐겨찾기에 추가하였습니다.")
+                    viewModel.addFavorite()
+                }
+            }
         }
     }
 }
