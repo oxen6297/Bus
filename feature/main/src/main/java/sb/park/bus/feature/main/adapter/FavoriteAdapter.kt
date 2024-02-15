9c8a6edc51676d65dc51a22dcc52bf9622ba82ea
oxen6297 <oxen6297@naver.com>
2024-02-15 18:22:05 +0900

    feat: adapter create

---
+++ feature/main/src/main/java/sb/park/bus/feature/main/adapter/FavoriteAdapter.kt
@@ -0,0 +1,47 @@
+package sb.park.bus.feature.main.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import sb.park.bus.feature.main.databinding.ItemFavoriteBinding
+import sb.park.model.response.FavoriteEntity
+
+class FavoriteAdapter : ListAdapter<FavoriteEntity, FavoriteAdapter.ViewHolder>(diffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemFavoriteBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.favorite = getItem(position)
+    }
+
+    class ViewHolder(val binding: ItemFavoriteBinding) : RecyclerView.ViewHolder(binding.root)
+
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<FavoriteEntity>() {
+            override fun areItemsTheSame(
+                oldItem: FavoriteEntity,
+                newItem: FavoriteEntity
+            ): Boolean {
+                return oldItem.busId == newItem.busId
+            }
+
+            override fun areContentsTheSame(
+                oldItem: FavoriteEntity,
+                newItem: FavoriteEntity
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
