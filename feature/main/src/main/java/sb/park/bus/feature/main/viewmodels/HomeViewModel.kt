21f472542d3d3768570ec89c4735a40d7a6ca06f
oxen6297 <oxen6297@naver.com>
2024-02-16 22:57:24 +0900

    feat: 즐겨찾기 삭제 및 데이터 출력 구현

--- feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/HomeViewModel.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/HomeViewModel.kt
@@ -3,17 +3,24 @@ package sb.park.bus.feature.main.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import sb.park.domain.usecases.BitCoinUseCase
+import sb.park.domain.usecases.FavoriteUseCase
 import sb.park.model.ApiResult
+import sb.park.model.response.FavoriteEntity
 import sb.park.model.successOrNull
 import javax.inject.Inject
 
 @HiltViewModel
-class BitCoinViewModel @Inject constructor(bitCoinUseCase: BitCoinUseCase) :
-    ViewModel() {
+class HomeViewModel @Inject constructor(
+    bitCoinUseCase: BitCoinUseCase,
+    private val favoriteUseCase: FavoriteUseCase
+) : ViewModel() {
 
     val uiState = bitCoinUseCase().stateIn(
         scope = viewModelScope,
@@ -26,4 +33,16 @@ class BitCoinViewModel @Inject constructor(bitCoinUseCase: BitCoinUseCase) :
         started = SharingStarted.WhileSubscribed(5000L),
         initialValue = null
     )
+
+    val favoriteFlow = favoriteUseCase.getFavorite().map { it.successOrNull() }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000L),
+        initialValue = null
+    )
+
+    fun deleteAll() {
+        viewModelScope.launch {
+            favoriteUseCase.deleteAll()
+        }
+    }
 }
