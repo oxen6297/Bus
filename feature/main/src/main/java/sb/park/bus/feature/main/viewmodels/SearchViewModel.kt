1438c54aa45fb89e2e35775461d3e16aba7ec286
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-03-12 09:15:48 +0900

    refactor: if -> when

--- feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/SearchViewModel.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/SearchViewModel.kt
@@ -43,12 +43,16 @@ class SearchViewModel @Inject constructor(
                         idState.successOrNull()?.forEach { response ->
                             busSearchUseCase(response.routeId.toString())
                                 .collectLatest { searchState ->
-                                    if (searchState is ApiResult.Error) {
-                                        _uiState.emit(ApiResult.Error(searchState.e))
-                                    }
-                                    if (searchState is ApiResult.Success) {
-                                        searchState.successOrNull()?.forEach {
-                                            searchList.add(it)
+                                    when (searchState) {
+                                        is ApiResult.Loading -> {}
+                                        is ApiResult.Error -> {
+                                            _uiState.emit(ApiResult.Error(searchState.e))
+                                        }
+
+                                        is ApiResult.Success -> {
+                                            searchState.successOrNull()?.forEach {
+                                                searchList.add(it)
+                                            }
                                         }
                                     }
                                 }
