454222f236414680dc0b2701c7c00b74f82123f9
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-03-12 11:57:58 +0900

    refactor: change SharingStarted whileSubscribed to lazily

--- feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/HomeViewModel.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/HomeViewModel.kt
@@ -3,16 +3,13 @@ package sb.park.bus.feature.main.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import sb.park.domain.usecases.BitCoinUseCase
 import sb.park.domain.usecases.FavoriteUseCase
 import sb.park.model.ApiResult
-import sb.park.model.response.FavoriteEntity
 import sb.park.model.successOrNull
 import javax.inject.Inject
 
@@ -36,7 +33,7 @@ class HomeViewModel @Inject constructor(
 
     val favoriteFlow = favoriteUseCase.getFavorite().map { it.successOrNull() }.stateIn(
         scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5000L),
+        started = SharingStarted.Lazily,
         initialValue = null
     )
 
