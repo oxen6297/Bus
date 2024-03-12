363cc62e26f54bd1ee7bb241039174e4c3d3d279
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-03-12 16:44:45 +0900

    feat: create check network state

--- feature/main/src/main/java/sb/park/bus/feature/main/views/activities/MainActivity.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/views/activities/MainActivity.kt
@@ -5,12 +5,18 @@ import dagger.hilt.android.AndroidEntryPoint
 import sb.park.bus.feature.main.R
 import sb.park.bus.feature.main.common.base.BaseActivity
 import sb.park.bus.feature.main.databinding.ActivityMainBinding
+import sb.park.bus.feature.main.extensions.showToast
+import sb.park.bus.feature.main.utils.CheckNetwork
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        CheckNetwork(this).observe(this) {
+            if (!it) {
+                showToast(getString(R.string.toast_network))
+            }
+        }
     }
 }
-
