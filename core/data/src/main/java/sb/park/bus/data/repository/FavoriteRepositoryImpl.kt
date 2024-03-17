e9b99919369de3b286bc5ab6a9bd9806fac5ac6a
oxen6297 <oxen6297@naver.com>
2024-03-17 13:40:59 +0900

    refactor: add coroutine dispatcher

--- core/data/src/main/java/sb/park/bus/data/repository/FavoriteRepositoryImpl.kt
+++ core/data/src/main/java/sb/park/bus/data/repository/FavoriteRepositoryImpl.kt
@@ -1,21 +1,27 @@
 package sb.park.bus.data.repository
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import sb.park.bus.data.AppDispatchers
+import sb.park.bus.data.Dispatcher
 import sb.park.bus.data.room.FavoriteDao
 import sb.park.model.ApiResult
 import sb.park.model.response.bus.FavoriteEntity
 import sb.park.model.safeFlow
 import javax.inject.Inject
 
-class FavoriteRepositoryImpl @Inject constructor(private val favoriteDao: FavoriteDao) :
-    FavoriteRepository {
+class FavoriteRepositoryImpl @Inject constructor(
+    private val favoriteDao: FavoriteDao,
+    @Dispatcher(AppDispatchers.IO) private val coroutineDispatcher: CoroutineDispatcher
+) : FavoriteRepository {
     override suspend fun insertFavorite(favoriteEntity: FavoriteEntity) {
         favoriteDao.insertFavorite(favoriteEntity)
     }
 
     override fun getFavorite(): Flow<ApiResult<List<FavoriteEntity>>> = safeFlow {
         favoriteDao.getFavorite()
-    }
+    }.flowOn(coroutineDispatcher)
 
     override suspend fun deleteFavorite(id: String) {
         favoriteDao.deleteFavorite(id)
