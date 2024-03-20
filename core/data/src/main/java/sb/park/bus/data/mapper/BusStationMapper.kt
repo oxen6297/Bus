e4ff2d32607fbf6ba90b3891c9b6314636c7aab0
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-03-20 10:41:16 +0900

    refactor: add this keyword

--- core/data/src/main/java/sb/park/bus/data/mapper/BusStationMapper.kt
+++ core/data/src/main/java/sb/park/bus/data/mapper/BusStationMapper.kt
@@ -4,18 +4,18 @@ import sb.park.model.BusType
 import sb.park.model.response.bus.BusStationResponse
 
 internal fun BusStationResponse.toData(): BusStationResponse = BusStationResponse(
-    busRouteNm = busRouteNm,
-    seq = seq,
-    stationId = stationId,
-    stationNm = stationNm,
-    gpsX = gpsX,
-    gpsY = gpsY,
-    direction = direction,
-    beginTm = beginTm,
-    lastTm = lastTm,
-    section = section,
+    busRouteNm = this.busRouteNm,
+    seq = this.seq,
+    stationId = this.stationId,
+    stationNm = this.stationNm,
+    gpsX = this.gpsX,
+    gpsY = this.gpsY,
+    direction = this.direction,
+    beginTm = this.beginTm,
+    lastTm = this.lastTm,
+    section = this.section,
     routeType = BusType.entries.find {
-        routeType == it.type
-    }?.typeName ?: routeType,
-    isTransfer = isTransfer
+        this.routeType == it.type
+    }?.typeName ?: this.routeType,
+    isTransfer = this.isTransfer
 )
