4dc8e6fdcf075c136e5aba1232965189b5e35ad4
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-03-20 15:24:08 +0900

    refactor: 불필요 parameter 제거

--- core/model/src/main/java/sb/park/model/response/bus/BusIdResponse.kt
+++ core/model/src/main/java/sb/park/model/response/bus/BusIdResponse.kt
@@ -4,12 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class BusIdResponse(
     @SerializedName("routeId") val routeId: Long,
-    @SerializedName("routeName") val routeName: String,
-    @SerializedName("Num") val num: Int,
-    @SerializedName("nodeId") val nodeId: Long,
-    @SerializedName("arsId") val arsId: String,
-
-    @SerializedName("stopName") val stopName: String,
-    @SerializedName("latitude") val latitude: Double,
-    @SerializedName("longitude") val longitude: Double
+    @SerializedName("routeName") val routeName: String
 )
