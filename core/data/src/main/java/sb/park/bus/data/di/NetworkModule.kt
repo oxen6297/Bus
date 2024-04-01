a9d130425a4e6d0080e687850b3ff365e4f0aac2
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-01 10:35:43 +0900

    refactor: delete call adapter

--- core/data/src/main/java/sb/park/bus/data/di/NetworkModule.kt
+++ core/data/src/main/java/sb/park/bus/data/di/NetworkModule.kt
@@ -9,7 +9,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import sb.park.bus.data.call.ResultCallAdapterFactory
 import sb.park.bus.data.service.BitCoinService
 import sb.park.bus.data.service.BusIdService
 import sb.park.bus.data.service.BusLocationService
@@ -49,7 +48,6 @@ internal object NetworkModule {
     fun provideBitCoinRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BIT_COIN)
         .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
-        .addCallAdapterFactory(ResultCallAdapterFactory())
         .client(okHttpClient)
         .build()
 
