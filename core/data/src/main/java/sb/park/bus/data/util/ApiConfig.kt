81a99c0bc02b0bdcf5f3fd7a164c2ae05dc7310c
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-08 16:32:16 +0900

    refactor: change network url

--- core/data/src/main/java/sb/park/bus/data/util/ApiConfig.kt
+++ core/data/src/main/java/sb/park/bus/data/util/ApiConfig.kt
@@ -9,13 +9,12 @@ internal const val BUS_ID = "https://raw.githubusercontent.com/"
 internal const val API_KEY =
     "rmu7X9qAYYph5vALPXikLGdHYOGhk3ZBgpYkGhMgiDtEAkEiJUkkV1UpwnQDVJsNBDA2aoJiIlpHq0xCpUM+Qw=="
 
-
 internal const val BITCOIN_SERVICE = "BTC_KRW"
 
 internal const val BUS_ID_SERVICE = "oxen6297/Bus/master/Bus.json"
 
 internal const val BUS_STATION_SERVICE = "busRouteInfo/getStaionByRoute"
 
-internal const val BUS_LOCATION_SERVICE = "buspos/getBusPosByRtidList"
+internal const val BUS_LOCATION_SERVICE = "buspos/getBusPosByRtid"
 
 internal const val RESULT_TYPE = "json"
