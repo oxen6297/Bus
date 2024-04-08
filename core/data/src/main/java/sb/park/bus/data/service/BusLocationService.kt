8ea3974fb54cffc793a81a319fdab84510d251cf
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-08 16:32:26 +0900

    refactor: change return type

--- core/data/src/main/java/sb/park/bus/data/service/BusLocationService.kt
+++ core/data/src/main/java/sb/park/bus/data/service/BusLocationService.kt
@@ -2,16 +2,16 @@ package sb.park.bus.data.service
 
 import retrofit2.http.GET
 import retrofit2.http.Query
-import sb.park.model.response.bus.BusLocationResponse
 import sb.park.bus.data.util.API_KEY
 import sb.park.bus.data.util.BUS_LOCATION_SERVICE
 import sb.park.bus.data.util.RESULT_TYPE
+import sb.park.model.response.bus.BusResponse
 
 interface BusLocationService {
     @GET(BUS_LOCATION_SERVICE)
     suspend fun getData(
-        @Query("serviceKey") serviceKey: String = API_KEY,
         @Query("busRouteId") busRouteId: String,
-        @Query("resultType") resultType: String = RESULT_TYPE,
-    ): List<BusLocationResponse>
+        @Query("serviceKey") serviceKey: String = API_KEY,
+        @Query("resultType") resultType: String = RESULT_TYPE
+    ): BusResponse
 }
