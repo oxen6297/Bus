0084ee61f57f5a398cb561092c6234b238dd6e4f
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-09 14:47:10 +0900

    refactor: insert bitCoin data

--- feature/main/src/main/java/sb/park/bus/feature/main/views/activities/SplashActivity.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/views/activities/SplashActivity.kt
@@ -2,18 +2,26 @@ package sb.park.bus.feature.main.views.activities
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import sb.park.bus.feature.main.R
 import sb.park.bus.feature.main.common.base.BaseActivity
 import sb.park.bus.feature.main.databinding.ActivitySplashBinding
+import sb.park.bus.feature.main.viewmodels.SplashViewModel
 
+@AndroidEntryPoint
 class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         lifecycleScope.launch {
+            viewModel.insertBitCoin()
             delay(500)
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
         }
