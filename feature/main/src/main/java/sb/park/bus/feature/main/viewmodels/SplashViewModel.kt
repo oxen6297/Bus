a88a9f88809a72a92bc2d015fda51a96c85b068c
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-09 14:46:27 +0900

    feat: create splashViewModel

---
+++ feature/main/src/main/java/sb/park/bus/feature/main/viewmodels/SplashViewModel.kt
@@ -0,0 +1,20 @@
+package sb.park.bus.feature.main.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import sb.park.domain.usecases.InsertBitCoinUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val insertBitCoinUseCase: InsertBitCoinUseCase
+) : ViewModel() {
+
+    fun insertBitCoin() {
+        viewModelScope.launch {
+            insertBitCoinUseCase()
+        }
+    }
+}
