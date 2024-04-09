db1d2b99e78a907d46273056224b30d7432b6700
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-09 16:58:11 +0900

    refactor: add param

--- core/model/src/main/java/sb/park/model/response/bitcoin/BitCoinResponse.kt
+++ core/model/src/main/java/sb/park/model/response/bitcoin/BitCoinResponse.kt
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName
 
 data class BitCoinResponse(
     @SerializedName("fluctate_rate_24H") val changeRatio: String,
+    @SerializedName("opening_price") val openPrice: String,
+    @SerializedName("closing_price") val closePrice: String,
     @SerializedName("max_price") val maxPrice: String,
     @SerializedName("min_price") val minPrice: String,
     @SerializedName("date") val date: String
