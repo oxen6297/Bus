3b01410c9ec459c99d12e2484ead69791cdff42f
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-09 16:56:39 +0900

    refactor: delete serialized

--- core/model/src/main/java/sb/park/model/response/bitcoin/BitCoinEntity.kt
+++ core/model/src/main/java/sb/park/model/response/bitcoin/BitCoinEntity.kt
@@ -3,7 +3,6 @@ package sb.park.model.response.bitcoin
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 
 @Entity
 data class BitCoinEntity(
