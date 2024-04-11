c1c370d3dadc1805bcbe74f8c4b27c8104771e9e
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-11 15:42:40 +0900

    refactor: 미사용 import 제거

--- core/data/src/main/java/sb/park/bus/data/repository/BitCoinRepository.kt
+++ core/data/src/main/java/sb/park/bus/data/repository/BitCoinRepository.kt
@@ -4,7 +4,6 @@ import com.github.mikephil.charting.data.CandleEntry
 import kotlinx.coroutines.flow.Flow
 import sb.park.model.ApiResult
 import sb.park.model.response.bitcoin.BaseResponse
-import sb.park.model.response.bitcoin.BitCoinEntity
 
 interface BitCoinRepository {
     fun getData(): Flow<ApiResult<BaseResponse>>
--- core/domain/src/main/java/sb/park/domain/usecases/GetChartUseCase.kt
+++ core/domain/src/main/java/sb/park/domain/usecases/GetChartUseCase.kt
@@ -4,7 +4,6 @@ import com.github.mikephil.charting.data.CandleEntry
 import kotlinx.coroutines.flow.Flow
 import sb.park.bus.data.repository.BitCoinRepository
 import sb.park.model.ApiResult
-import sb.park.model.response.bitcoin.BitCoinEntity
 import javax.inject.Inject
 
 class GetChartUseCase @Inject constructor(private val bitCoinRepository: BitCoinRepository) {
