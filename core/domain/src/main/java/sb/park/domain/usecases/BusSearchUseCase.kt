00faa837ac8e4cebc29aabc93a7af27df45c8c07
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-11 14:42:24 +0900

    refactor: search repository 적용

--- core/domain/src/main/java/sb/park/domain/usecases/BusSearchUseCase.kt
+++ core/domain/src/main/java/sb/park/domain/usecases/BusSearchUseCase.kt
@@ -1,13 +1,13 @@
 package sb.park.domain.usecases
 
 import kotlinx.coroutines.flow.Flow
+import sb.park.bus.data.repository.BusSearchRepository
 import sb.park.model.ApiResult
-import sb.park.bus.data.repository.BusStationRepository
 import sb.park.model.response.bus.BusSearchResponse
 import javax.inject.Inject
 
-class BusSearchUseCase @Inject constructor(private val busStationRepository: BusStationRepository) {
+class BusSearchUseCase @Inject constructor(private val busSearchRepository: BusSearchRepository) {
 
-    operator fun invoke(busId: String): Flow<ApiResult<List<BusSearchResponse>>> =
-        busStationRepository.getSearch(busId)
+    operator fun invoke(busNumber: String): Flow<ApiResult<List<BusSearchResponse>>> =
+        busSearchRepository.getSearch(busNumber)
 }
