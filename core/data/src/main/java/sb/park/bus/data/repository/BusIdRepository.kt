38ee1278ad73cd41646905fc2a766934d826942e
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-11 14:40:43 +0900

    remove: 미사용 file 제거

--- core/data/src/main/java/sb/park/bus/data/repository/BusIdRepository.kt
+++
@@ -1,9 +0,0 @@
-package sb.park.bus.data.repository
-
-import kotlinx.coroutines.flow.Flow
-import sb.park.model.ApiResult
-import sb.park.model.response.bus.BusIdResponse
-
-interface BusIdRepository {
-    fun getData(busNumber: String): Flow<ApiResult<List<BusIdResponse>>>
-}
