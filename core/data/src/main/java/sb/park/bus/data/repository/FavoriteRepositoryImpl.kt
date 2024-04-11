58001c7e5b024ab30290c9276b27a3a9eb5c4202
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-04-11 14:42:39 +0900

    refactor: add internal keyword

--- core/data/src/main/java/sb/park/bus/data/repository/FavoriteRepositoryImpl.kt
+++ core/data/src/main/java/sb/park/bus/data/repository/FavoriteRepositoryImpl.kt
@@ -4,7 +4,7 @@ import sb.park.bus.data.room.FavoriteDao
 import sb.park.model.response.bus.FavoriteEntity
 import javax.inject.Inject
 
-class FavoriteRepositoryImpl @Inject constructor(
+internal class FavoriteRepositoryImpl @Inject constructor(
     private val favoriteDao: FavoriteDao
 ) : FavoriteRepository {
     override suspend fun insertFavorite(favoriteEntity: FavoriteEntity) {
