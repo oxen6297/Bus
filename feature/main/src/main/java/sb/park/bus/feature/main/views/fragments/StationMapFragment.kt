d9e4fd4660bc348c3be7f41d8cb530278fca1f85
oxen6297 <oxen6297@naver.com>
2024-05-13 20:29:41 +0900

    feat: create map fragment

---
+++ feature/main/src/main/java/sb/park/bus/feature/main/views/fragments/StationMapFragment.kt
@@ -0,0 +1,19 @@
+package sb.park.bus.feature.main.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import sb.park.bus.feature.main.R
+import sb.park.bus.feature.main.common.base.BaseFragment
+import sb.park.bus.feature.main.databinding.FragmentStationMapBinding
+
+class StationMapFragment : BaseFragment<FragmentStationMapBinding>(R.layout.fragment_station_map) {
+
+    override fun initView(view: View) {
+        bind {
+
+        }
+    }
+}
