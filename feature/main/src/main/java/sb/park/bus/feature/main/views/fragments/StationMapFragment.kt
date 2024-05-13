e1338051193124b13e4ed0720cf88128f9dcd301
oxen6297 <oxen6297@naver.com>
2024-05-14 00:32:41 +0900

    refactor: mapView 객체 초기화

--- feature/main/src/main/java/sb/park/bus/feature/main/views/fragments/StationMapFragment.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/views/fragments/StationMapFragment.kt
@@ -5,15 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.naver.maps.map.MapView
 import sb.park.bus.feature.main.R
 import sb.park.bus.feature.main.common.base.BaseFragment
 import sb.park.bus.feature.main.databinding.FragmentStationMapBinding
 
 class StationMapFragment : BaseFragment<FragmentStationMapBinding>(R.layout.fragment_station_map) {
 
+    private lateinit var mapView: MapView
+
     override fun initView(view: View) {
         bind {
-
+            mapView = binding.mapview
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
