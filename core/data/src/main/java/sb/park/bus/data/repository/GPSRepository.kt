e87acc3e7eda470aa7692b2058cb51fa5c16b001
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-05-14 13:40:33 +0900

    feat: create gps data / domain / viewModel

---
+++ core/data/src/main/java/sb/park/bus/data/repository/GPSRepository.kt
@@ -0,0 +1,10 @@
+package sb.park.bus.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import sb.park.model.ApiResult
+import sb.park.model.response.bus.GPSModel
+
+interface GPSRepository {
+
+    fun getLastLocation(): Flow<ApiResult<GPSModel>>
+}
---
+++ core/domain/src/main/java/sb/park/domain/usecases/GPSUseCase.kt
@@ -0,0 +1,12 @@
+package sb.park.domain.usecases
+
+import kotlinx.coroutines.flow.Flow
+import sb.park.bus.data.repository.GPSRepository
+import sb.park.model.ApiResult
+import sb.park.model.response.bus.GPSModel
+import javax.inject.Inject
+
+class GPSUseCase @Inject constructor(private val gpsRepository: GPSRepository) {
+
+    operator fun invoke(): Flow<ApiResult<GPSModel>> = gpsRepository.getLastLocation()
+}
