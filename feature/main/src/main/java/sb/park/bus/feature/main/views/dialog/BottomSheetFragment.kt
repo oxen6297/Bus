5752754150d85ab5f9f202b65d7407c8cfe06554
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-05-17 16:46:23 +0900

    feat: create bottomSheetDialogFragment

---
+++ feature/main/src/main/java/sb/park/bus/feature/main/views/dialog/BottomSheetFragment.kt
@@ -0,0 +1,32 @@
+package sb.park.bus.feature.main.views.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import sb.park.bus.feature.main.databinding.FragmentBottomSheetBinding
+
+class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentBottomSheetBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
