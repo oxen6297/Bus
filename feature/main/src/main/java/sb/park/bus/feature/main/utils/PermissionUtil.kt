fc71c3eb8eaba50213dd8f8172e2ac237b93d8d6
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-05-17 15:38:14 +0900

    refactor: toast -> dialog

--- feature/main/src/main/java/sb/park/bus/feature/main/utils/PermissionUtil.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/utils/PermissionUtil.kt
@@ -9,43 +9,41 @@ import android.net.Uri
 import android.provider.Settings
 import androidx.core.content.ContextCompat
 import sb.park.bus.feature.main.R
-import sb.park.bus.feature.main.extensions.showToast
+import sb.park.bus.feature.main.extensions.customDialog
 
 object PermissionUtil {
 
     fun checkPermission(context: Context): Boolean {
-        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val permissionList = arrayOf(
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
-
-        val isGranted = permissionList.all {
-            ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
-        }
-
-        if (!isGranted) {
-            val intent = Intent(
-                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                Uri.parse("package:${context.packageName}")
+        context.apply {
+            val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val permissionList = arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
             )
 
-            context.apply {
-                showToast(getString(R.string.toast_location_setting))
-                startActivity(intent)
+            val isGranted = permissionList.all {
+                ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
             }
 
-            return false
-        }
-
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            context.apply {
-                showToast(getString(R.string.toast_gps))
-                startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            if (!isGranted) {
+                customDialog(getString(R.string.dialog_location_setting)) {
+                    val intent = Intent(
+                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.parse("package:${packageName}")
+                    )
+                    startActivity(intent)
+                }
+                return false
             }
-            return false
-        }
 
-        return true
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                customDialog(getString(R.string.dialog_gps)) {
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+                return false
+            }
+
+            return true
+        }
     }
 }
