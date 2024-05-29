84e0885598080f5028b178b95a3822181fd22bc0
oxen6297 <oxen6297@naver.com>
2024-05-30 00:18:37 +0900

    refactor: 위경도 수정

--- core/model/src/main/java/sb/park/model/response/bus/GPSModel.kt
+++ core/model/src/main/java/sb/park/model/response/bus/GPSModel.kt
@@ -1,6 +1,6 @@
 package sb.park.model.response.bus
 
 data class GPSModel(
-    val latitude: Double = 126.972591728,
-    val longitude: Double = 37.552987017
+    val latitude: Double = 37.552987017,
+    val longitude: Double = 126.972591728
 )
