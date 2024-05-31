843d7920d96e7a4de750b3de95d464835d825917
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-05-31 14:27:18 +0900

    feat: create bottomSheet extension

--- feature/main/src/main/java/sb/park/bus/feature/main/extensions/Extensions.kt
+++ feature/main/src/main/java/sb/park/bus/feature/main/extensions/Extensions.kt
@@ -3,10 +3,14 @@ package sb.park.bus.feature.main.extensions
 import android.content.Context
 import android.view.View
 import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import sb.park.bus.feature.main.R
 import sb.park.bus.feature.main.utils.CustomDialogBuilder
 import sb.park.bus.feature.main.utils.SingleClick
 
+/**
+ * View Extension
+ */
 fun View.hide() {
     visibility = View.GONE
 }
@@ -15,12 +19,16 @@ fun View.show() {
     visibility = View.VISIBLE
 }
 
-fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, message, duration).show()
-}
-
 fun View.singleClickListener(click: (View) -> Unit) {
     setOnClickListener(SingleClick(click))
+}
+
+
+/**
+ * Context Extension
+ */
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
 }
 
 fun Context.customDialog(
@@ -36,3 +44,17 @@ fun Context.customDialog(
     clickConfirm(clickConfirm)
     showDialog()
 }
+
+
+/**
+ * BottomSheet Extension
+ */
+inline fun <T : View> BottomSheetBehavior<T>.setOnSlide(
+    crossinline onSlide: (View, Float) -> Unit
+) = addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+    override fun onStateChanged(p0: View, p1: Int) {}
+
+    override fun onSlide(p0: View, p1: Float) {
+        onSlide(p0, p1)
+    }
+})
